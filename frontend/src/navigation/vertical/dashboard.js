32679b9ee9a4942b9997b8ebd55b1a8e0aa05c31
sergeinikolaev <ggeser@gmail.com>
2021-08-23 23:01:50 +0300

    Переделываю вертикальное меню

--- frontend/src/navigation/vertical/dashboard.js
+++ frontend/src/navigation/vertical/dashboard.js
@@ -1,18 +1,27 @@
 export default [
   {
-    title: 'Dashboards',
-    icon: 'HomeIcon',
-    tag: '2',
-    tagVariant: 'light-warning',
-    children: [
-      {
-        title: 'eCommerce',
-        route: 'dashboard-ecommerce',
-      },
-      {
-        title: 'Analytics',
-        route: 'dashboard-analytics',
-      },
-    ],
+    header: 'Main',
   },
+  {
+    title: 'Dashboard',
+    route: 'dashboard-ecommerce',
+    icon: 'HomeIcon',
+  },
+
+  // {
+  //   title: 'Dashboards',
+  //   icon: 'HomeIcon',
+  //   tag: '2',
+  //   tagVariant: 'light-warning',
+  //   children: [
+  //     {
+  //       title: 'eCommerce',
+  //       route: 'dashboard-ecommerce',
+  //     },
+  //     {
+  //       title: 'Analytics',
+  //       route: 'dashboard-analytics',
+  //     },
+  //   ],
+  // },
 ]
